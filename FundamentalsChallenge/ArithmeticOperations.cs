662ac7a5aca9c68f101d91f5b9e80d7c906250f8
A. Dartagnan Junior <dartajunior3@gmail.com>
2025-05-08 13:17:37 -0300

    feature: implement ArithmeticOperations class for basic arithmetic functions

---
+++ FundamentalsChallenge/ArithmeticOperations.cs
@@ -0,0 +1,46 @@
+﻿namespace FundamentalsChallenge
+{
+  public class ArithmeticOperations
+  {
+    private readonly double firstNumber;
+    private readonly double secondNumber;
+
+    public ArithmeticOperations(double firstNumber, double secondNumber)
+    {
+      this.firstNumber = firstNumber;
+      this.secondNumber = secondNumber;
+    }
+
+    public void PrintSum()
+    {
+      Console.WriteLine($"Sum: {firstNumber + secondNumber}");
+    }
+
+    public void PrintSubtraction()
+    {
+      Console.WriteLine($"Subtraction: {firstNumber - secondNumber}");
+    }
+
+    public void PrintMultiplication()
+    {
+      Console.WriteLine($"Multiplication: {firstNumber * secondNumber}");
+    }
+
+    public void PrintDivision()
+    {
+      if (secondNumber != 0)
+      {
+        Console.WriteLine($"Division: {firstNumber / secondNumber}");
+      }
+      else
+      {
+        Console.WriteLine("Division by zero is not allowed.");
+      }
+    }
+
+    public void PrintMean()
+    {
+      Console.WriteLine($"Mean: {(firstNumber + secondNumber) / 2}");
+    }
+  }
+}
--- FundamentalsChallenge/Program.cs
+++ FundamentalsChallenge/Program.cs
@@ -8,6 +8,13 @@
       var WelcomeMessage = new WelcomeMessage();
       WelcomeMessage.PrintWelcomeMessage();
       WelcomeMessage.PrintFullName();
+
+      var arithmeticOperations = new ArithmeticOperations(10, 5);
+      arithmeticOperations.PrintSum();
+      arithmeticOperations.PrintSubtraction();
+      arithmeticOperations.PrintMultiplication();
+      arithmeticOperations.PrintDivision();
+      arithmeticOperations.PrintMean();
     }
   }
 }
