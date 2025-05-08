c11f8569a266ca4f9fd51ca9218272d181d8f386
A. Dartagnan Junior <dartajunior3@gmail.com>
2025-05-08 12:33:42 -0300

    feature: add initial project files including .gitignore, solution, and main program

---
+++ FundamentalsChallenge/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Class1
+{
+    static void Main() {
+        Console.WriteLine("Please inform your first name.");
+        var firstName = Console.ReadLine();
+        Console.WriteLine($"Welcome to our Program {firstName}");
+        Console.WriteLine("Please inform your last name.");
+        var lastName = Console.ReadLine();
+        Console.WriteLine($"Thats your full name: {firstName} {lastName}");
+        Console.WriteLine("Press any key to exit…");
+        Console.ReadKey();
+    }
+
+}
