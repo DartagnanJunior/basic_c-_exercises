4f67b9747b1398be2a1a061b6b8cdae5cde8046f
A. Dartagnan Junior <dartajunior3@gmail.com>
2025-05-08 13:07:12 -0300

    refactor: restructure Program class and add WelcomeMessage class for improved user interaction

--- FundamentalsChallenge/Program.cs
+++ FundamentalsChallenge/Program.cs
@@ -1,16 +1,13 @@
-﻿using System;
+﻿namespace FundamentalsChallenge
 
-public class Class1
 {
-    static void Main() {
-        Console.WriteLine("Please inform your first name.");
-        var firstName = Console.ReadLine();
-        Console.WriteLine($"Welcome to our Program {firstName}");
-        Console.WriteLine("Please inform your last name.");
-        var lastName = Console.ReadLine();
-        Console.WriteLine($"Thats your full name: {firstName} {lastName}");
-        Console.WriteLine("Press any key to exit…");
-        Console.ReadKey();
+  public class Program
+  {
+    static void Main()
+    {
+      var WelcomeMessage = new WelcomeMessage();
+      WelcomeMessage.PrintWelcomeMessage();
+      WelcomeMessage.PrintFullName();
     }
-
+  }
 }
---
+++ FundamentalsChallenge/WelcomeMessage.cs
@@ -0,0 +1,23 @@
+﻿namespace FundamentalsChallenge
+{
+  public class WelcomeMessage
+  {
+    string? firstName;
+
+    public void PrintWelcomeMessage()
+    {
+      Console.WriteLine("Please inform your first name.");
+      firstName = Console.ReadLine();
+      Console.WriteLine($"Welcome to our Program {firstName}");
+    }
+
+    public void PrintFullName()
+    {
+      Console.WriteLine("Please inform your first name.");
+      firstName = Console.ReadLine();
+      Console.WriteLine("Please inform your last name.");
+      var lastName = Console.ReadLine();
+      Console.WriteLine($"Thats your full name: {firstName} {lastName}");
+    }
+  }
+}
