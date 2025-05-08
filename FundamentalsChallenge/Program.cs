710d09a2cbe464ee44b8f2e0375f492a792643ef
A. Dartagnan Junior <dartajunior3@gmail.com>
2025-05-08 13:41:10 -0300

    feature: add VehicleLicensePlate class with validation functionality for license plates

--- FundamentalsChallenge/Program.cs
+++ FundamentalsChallenge/Program.cs
@@ -5,18 +5,20 @@
   {
     static void Main()
     {
-      var WelcomeMessage = new WelcomeMessage();
-      WelcomeMessage.PrintWelcomeMessage();
-      WelcomeMessage.PrintFullName();
+      //var WelcomeMessage = new WelcomeMessage();
+      //WelcomeMessage.PrintWelcomeMessage();
+      //WelcomeMessage.PrintFullName();
 
-      var arithmeticOperations = new ArithmeticOperations(10, 5);
-      arithmeticOperations.PrintSum();
-      arithmeticOperations.PrintSubtraction();
-      arithmeticOperations.PrintMultiplication();
-      arithmeticOperations.PrintDivision();
-      arithmeticOperations.PrintMean();
+      //var arithmeticOperations = new ArithmeticOperations(10, 5);
+      //arithmeticOperations.PrintSum();
+      //arithmeticOperations.PrintSubtraction();
+      //arithmeticOperations.PrintMultiplication();
+      //arithmeticOperations.PrintDivision();
+      //arithmeticOperations.PrintMean();
 
-      WordCharacterCounter.WordCharacterCounterPrint();
+      //WordCharacterCounter.WordCharacterCounterPrint();
+
+      var validPlate = VehicleLicensePlate.ValidatePlate();
     }
   }
 }
---
+++ FundamentalsChallenge/VehicleLicensePlate.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FundamentalsChallenge
+{
+  public class VehicleLicensePlate
+  {
+
+    public static bool ValidatePlate()
+    {
+      Console.WriteLine("Please enter a vehicle license plate: ");
+      var plate = Console.ReadLine();
+
+      if (plate == null)
+      {
+        Console.WriteLine("Invalid plate. You did not enter a license plate.");
+        return false;
+      }
+
+      if (plate.Length != 7)
+      {
+        Console.WriteLine("Invalid plate. The license plate must be 7 characters long.");
+        return false;
+      }
+
+      var regex = new Regex(@"^[A-Za-z]{3}[0-9]{4}$");
+      if (!regex.IsMatch(plate))
+      {
+        Console.WriteLine("Invalid plate. The license plate must have 3 letters followed by 4 numbers.");
+        return false;
+      }
+
+      Console.WriteLine("Valid license plate!");
+      return true;
+    }
+  }
+}
